e89fe47c84add7ec1dd7059c14445e4b86958d1f
drestew <drestew48@gmail.com>
2022-06-24 20:44:35 -0400

    Add db configuration

--- server.js
+++ server.js
@@ -3,7 +3,22 @@ const app = express()
 const cors = require('cors')
 const PORT = 8000
 require('dotenv').config
+const MongoClient = require('mongodb').MongoClient
 
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'nutrition-facts'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.use(cors())
 
 const food = {
