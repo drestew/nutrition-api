2b7fd091662a17d4527db81a2c7eb412d23fd82a
drestew <drestew48@gmail.com>
2022-06-25 22:43:24 -0400

    Add post request and ejs template

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
+const { response, request } = require('express')
 const express = require('express')
 const app = express()
-const cors = require('cors')
 const PORT = 8000
-require('dotenv').config
+require('dotenv').config()
 const MongoClient = require('mongodb').MongoClient
 
 let db,
@@ -19,46 +19,26 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(cors())
 
-const food = {
-    'teryaki chicken': {
-        'fat': '4g',
-        'carbs': '17g',
-        'protein': '32g',
-        'calories': '140',
-    },
-
-    'cheesburger': {
-        'fat': '16g',
-        'carbs': '29g',
-        'protein': '24g',
-        'calories': '370',
-    },
-
-    'potato salad': {
-        'fat': '8g',
-        'carbs': '43g',
-        'protein': '4g',
-        'calories': '230',
-    },
-
-    'unknown': {
-        'fat': '0g',
-        'carbs': '0g',
-        'protein': '0g',
-        'calories': '0g',
-    },
-}
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+    db.collection('nutrition-facts').find().sort({ likes: -1 }).toArray()
+        .then(data => {
+            response.render('index.ejs', { info: data })
+        })
+        .catch(error => console.error(error))
 })
 
-app.get('/api/:food', (req, res) => {
-    const foodItem = req.params.food
-    console.log(food[foodItem])
-    res.json(food)
+app.post('/addFood', (req, res) => {
+    db.collection('nutrition-facts').insertOne({
+        name: req.body.name,
+        calories: req.body.calories,
+        protein: req.body.protein,
+        carbs: req.body.carbs,
+        fat: req.body.fat,
+        likes: 0,
+    })
+        .then(res.redirect('/'))
 })
 
 app.listen(process.env.PORT || PORT, () => {
